766a8fcde42b8060449e2e232ebc5cec49168122
akgithub1234 <105662538+akgithub1234@users.noreply.github.com>
2022-05-17 00:00:29 +0530

    Update QuoteClient.java

--- 1A UDP/QuoteClient.java
+++ 1A UDP/QuoteClient.java
@@ -1,47 +1,13 @@
-import java.io.*;
-import java.net.*;
- 
-
-public class QuoteClient {
- 
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Syntax: QuoteClient <hostname> <port>");
-            return;
-        }
- 
-        String hostname = args[0];
-        int port = Integer.parseInt(args[1]);
- 
-        try {
-            InetAddress address = InetAddress.getByName(hostname);
-            DatagramSocket socket = new DatagramSocket();
- 
-            while (true) {
- 
-                DatagramPacket request = new DatagramPacket(new byte[1], 1, address, port);
-                socket.send(request);
- 
-                byte[] buffer = new byte[512];
-                DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-                socket.receive(response);
- 
-                String quote = new String(buffer, 0, response.getLength());
- 
-                System.out.println(quote);
-                System.out.println();
- 
-                Thread.sleep(3000);
-            }
- 
-        } catch (SocketTimeoutException ex) {
-            System.out.println("Timeout error: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            System.out.println("Client error: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-    }
-}
+//DReceiver.java  
+import java.net.*;  
+public class DReceiver{  
+  public static void main(String[] args) throws Exception {  
+    DatagramSocket ds = new DatagramSocket(3000);  
+    byte[] buf = new byte[1024];  
+    DatagramPacket dp = new DatagramPacket(buf, 1024);  
+    ds.receive(dp);  
+    String str = new String(dp.getData(), 0, dp.getLength());  
+    System.out.println(str);  
+    ds.close();  
+  }  
+}  
